491a5fa391f1a901d47d26874221a757d27be0ba
MaxMonax <maxmonax@gmail.com>
2018-08-28 21:09:42 +0500

    new Phaser version, improve ObjAvgValue logic

--- VSCodeTemplate/VSCodeProj/src/utils/ObjAvgValueUtils.ts
+++ VSCodeTemplate/VSCodeProj/src/utils/ObjAvgValueUtils.ts
@@ -1,109 +1,105 @@
 ﻿class ObjAvgValueUtils {
-	private game: Phaser.Game;
+  private game: Phaser.Game;
+  // объекты с зависимостью от процентов
+  private itemsPerc: Array<any> = [];
+  // объекты с зависимостью от значений
+  private itemsVals: Array<any> = [];
 
-	private gh = Config.GH;
-	private gsh = Config.GSH;
-	private press_perc_y: number = 0; // процент сжатия до safe area по Y
+  private updateTimer: number = 0;
+  private UPD_TIME = 0.1; // sec
 
-	private itemsYPos: Array<any> = [];
-	private itemsScale: Array<any> = [];
-	private itemsUni: Array<any> = [];
+  constructor(aGame: Phaser.Game) {
+    this.game = aGame;
+    this.itemsPerc = [];
+    this.itemsVals = [];
+  }
 
-	private updateTimer: number = 0;
-	private UPD_TIME = 0.1; // sec
+  private getValueFromPerc(aVals: Array<any>, aPerc: number): number {
+    var res = 0;
+    var p = aPerc;
+    var minData: any;
+    var maxData: any;
+    for (var i = 0; i < aVals.length; i++) {
+      if (aVals[i].p <= aPerc && (!minData || aVals[i].p > minData.p)) {
+        minData = aVals[i];
+      }
+      if (aVals[i].p > aPerc && (!maxData || aVals[i].p < maxData.p)) {
+        maxData = aVals[i];
+      }
+    }
 
-	constructor(aGame: Phaser.Game) {
-		this.game = aGame;
-	}
+    if (minData && maxData) {
+      var v1 = minData.v;
+      var p1 = minData.p;
+      var v2 = maxData.v;
+      var p2 = maxData.p;
+      res = v1 + (v2 - v1) * (p - p1) / (p2 - p1);
+    }
+    else if (minData) {
+      res = minData.v;
+    }
+    else if (maxData) {
+      res = maxData.v;
+    }
+    //console.log('==================');
+    //console.log('p = ' + p);
+    //console.log('p1 = ' + p1);
+    //console.log('v1 = ' + v1);
+    //console.log('p2 = ' + p2);
+    //console.log('v2 = ' + v2);
+    //console.log('res = ' + res);
+    return res;
+  }
 
-	private getValue(aVals: Array<any>, aPerc: number): number {
-		var res = 0;
-		var p = aPerc;
-		var p1 = 0;
-		var v1 = 0;
-		var p2 = 0;
-		var v2 = 0;
-		for (var i = 0; i < aVals.length; i++) {
-			if (aVals[i].p <= aPerc) {
-				p1 = aVals[i].p;
-				v1 = aVals[i].v;
-			}
-			else {
-				p2 = aVals[i].p;
-				v2 = aVals[i].v;
-				break;
-			}
-		}
+  private updatePercObj(aObjData: any) {
+    var min = aObjData.min;
+    var max = aObjData.max;
+    var cval = aObjData.ov[aObjData.fv];
+    // расчёт процента значения
+    var perc = 100 * (cval - min) / (max - min);
+    aObjData.o[aObjData.f] = this.getValueFromPerc(aObjData.vals, perc);
+  }
 
-		res = v1 + (v2 - v1) * (p - p1) / (p2 - p1);
+  /**
+	 * Добавления зависимости значения поля объекта от процентов сжатия значений второго объекта.
+   * @param aObj зависимый объект
+   * @param aField изменяемое поле объекта
+   * @param aMax максимальное значение основной величины
+   * @param aMin минимальное значение
+   * @param aObjVal объект величины
+   * @param aFieldVal поле значения
+   * @param aVals массив значений для процентов значения: [{p: 0, v: 120}, {p: 100, v: 230}]
+   */
+  addItemPercVals(aObj: any, aField: string, aMax: number, aMin: number, aObjVal, aFieldVal: string, aVals: Array<any>) {
+    this.itemsPerc.push({ o: aObj, f: aField, min: aMin, max: aMax, ov: aObjVal, fv: aFieldVal, vals: aVals });
+  }
 
-		//console.log('==================');
-		//console.log('p = ' + p);
-		//console.log('p1 = ' + p1);
-		//console.log('v1 = ' + v1);
-		//console.log('p2 = ' + p2);
-		//console.log('v2 = ' + v2);
-		//console.log('res = ' + res);
+  removeItemPerc(aObj: any) {
+    for (var i = this.itemsPerc.length - 1; i >= 0; i--) {
+      try {
+        if (this.itemsPerc[i]['o'] == aObj)
+          this.itemsPerc.splice(i, 1);
+      }
+      catch (e) {
+        LogMng.error('ObjAvgValueUtils.removeItem: ' + e);
+      }
+    }
+  }
 
-		return res;
-	}
+  update() {
+    var dt = this.game.time.elapsed * 0.001;
+    this.updateTimer -= dt;
+    if (this.updateTimer > 0) return;
+    this.updateTimer = this.UPD_TIME;
 
-	getValueFromVals(vals: Array<any>): number {
-		return this.getValue(vals, this.press_perc_y * 100);
-	}
-
-	/**
-	 * 
-	 * @param aObj
-	 * @param aField - изменяемое поле объекта
-	 * @param aVals - массив значений для процентов: [{p: 0, v: 120}, {p: 100, v: 230}]
-	 */
-	addItem(aObj: any, aField: string, aVals: Array<any>) {
-		this.itemsUni.push({ o: aObj, f: aField, v: aVals });
-	}
-
-	removeItem(aObj: any) {
-		for (var i = this.itemsUni.length - 1; i >= 0; i--) {
-			try {
-				if (this.itemsUni[i]['o'] == aObj)
-					this.itemsUni.splice(i, 1);
-			}
-			catch (e) {
-				LogMng.error('ObjAvgValueUtils.removeItem: ' + e);
-			}
-		}
-	}
-
-	update(dt: number) {
-
-		var gvh = ScaleManager.gameViewH;
-		this.press_perc_y = (this.gh - gvh) / (this.gh - this.gsh); // процент сжатия до safe area по Y
-
-		this.updateTimer -= dt;
-		if (this.updateTimer > 0) return;
-		this.updateTimer = this.UPD_TIME;
-		
-		// позиционирование
-		//for (var i = 0; i < this.itemsYPos.length; i++) {			
-		//	var obj: any = this.itemsYPos[i].obj;
-		//	var y0 = this.itemsYPos[i].y0;
-		//	var sa_y = this.itemsYPos[i].ysa;
-		//	var dy = sa_y - y0;
-		//	obj.y = y0 + dy * perc;
-		//}
-		
-		for (var i = this.itemsUni.length - 1; i >= 0; i--) {
-			try {
-				var obj: any = this.itemsUni[i].o;
-				obj[this.itemsUni[i].f] = this.getValueFromVals(this.itemsUni[i].v);
-				obj = null;
-			}
-			catch (e) {
-				//LogMng.warn('obj == null, deleted...');
-				this.itemsUni.splice(i, 1);
-			}
-		}
-
-	}
+    for (var i = this.itemsPerc.length - 1; i >= 0; i--) {
+      try {
+        this.updatePercObj(this.itemsPerc[i]);
+      }
+      catch (e) {
+        this.itemsPerc.splice(i, 1);
+      }
+    }
+  }
 
 }
